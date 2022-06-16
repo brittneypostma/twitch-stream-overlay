5d37a4f3a43b595eee864055cfcd197ee1c86c4c
brittneypostma <brittneypostma@BrittneysDesk.localdomain>
2022-06-16 10:36:24 -0400

    init

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,10 @@
+<script>
+	import '$lib/styles.css';
+	import '$lib/components/Bubbles.svelte';
+	import Bubbles from '../lib/components/Bubbles.svelte';
+</script>
+
+<main>
+	<slot />
+</main>
+<Bubbles />
