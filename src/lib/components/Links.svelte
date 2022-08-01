56dea2fe247e99488e0a3b5768ea772b75491c9f
brittneypostma <bdesignedwebdev@gmail.com>
2022-08-01 09:47:53 -0400

    added a bunch of images and links to stream overlay

---
+++ src/lib/components/Links.svelte
@@ -0,0 +1,56 @@
+<footer>
+	<img class="bg" src="bg.png" alt="Brittney Postma on a wave" />
+	<section>
+		<a href="brittneypostma.com">
+			<img src="logo.png" alt="b in a wave shape" />
+			BrittneyPostma.com
+		</a>
+		<a href="codingcat.dev">
+			<img src="coding-cat.png" alt="Black cat with a rainbow tag with AJ on it" />
+			CodingCat.dev
+		</a>
+		<a href="github.com/brittneypostma">
+			<img src="github.png" alt="github logo on a palm leaf" />
+			Github.com/BrittneyPostma
+		</a>
+		<a href="twitter.com/brittneypostma">
+			<img src="twitter.png" alt="Twitter logo on a palm leaf" />
+			Twitter.com/BrittneyPostma
+		</a>
+	</section>
+</footer>
+
+<style>
+	section {
+		display: grid;
+		justify-items: start;
+		position: absolute;
+		bottom: 0;
+		left: 0;
+		padding: 0 0 1rem 10rem;
+	}
+
+	.bg {
+		position: absolute;
+		bottom: 0;
+		left: 0;
+		width: 300px;
+		z-index: -1;
+	}
+
+	a {
+		display: flex;
+		gap: 0.5rem;
+		align-items: center;
+		justify-content: center;
+	}
+	a:hover {
+		border-bottom: none;
+	}
+	img {
+		width: 30px;
+		display: grid;
+		align-items: center;
+		justify-content: center;
+	}
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,6 +1,8 @@
 <script>
 	import '$lib/components/Bubbles.svelte';
-	import Bubbles from '../lib/components/Bubbles.svelte';
+	import Bubbles from '$lib/components/Bubbles.svelte';
+	import Links from '$lib/components/Links.svelte';
 </script>
 
 <Bubbles />
+<Links />
